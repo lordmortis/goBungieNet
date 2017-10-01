69c24aa291bb887a4a218218cc55e0fef232756d
Brendan Ragan <lordmortis@gmail.com>
2017-10-01 14:02:37 +0800

    Query multiple hashes of activities!

--- manifestActivities.go
+++ manifestActivities.go
@@ -1,104 +1,161 @@
 package goBungieNet
 
 import (
-  "fmt"
-  "errors"
-  "time"
-  "encoding/json"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
 )
 
 type DestinyActivityReward struct {
-  Text string `json:rewardText`
-  Items []DestinyItemQuantity `json:rewardItems`
+	Text  string                `json:rewardText`
+	Items []DestinyItemQuantity `json:rewardItems`
 }
 
 type DestinyActivityModifier struct {
-  ActivityModifierHash uint32
+	ActivityModifierHash uint32
 }
 
 type DestinyActivityChallenges struct {
-  ObjectiveHash uint32
+	ObjectiveHash uint32
 }
 
 type DestinyActivityUnlockString struct {
-  DisplayString string
+	DisplayString string
 }
 
 type DestinyActivityPlaylistItem struct {
-  ActivityHash uint32
-  DirectActivityModeHash uint32
-  DirectActivityModeType DestinyActivityMode
-  ActivityModeHashes []uint32
-  ActivityModeTypes []DestinyActivityMode
+	ActivityHash           uint32
+	DirectActivityModeHash uint32
+	DirectActivityModeType DestinyActivityMode
+	ActivityModeHashes     []uint32
+	ActivityModeTypes      []DestinyActivityMode
 }
 
 type DestinyActivityMatchmaking struct {
-  IsMatchmade bool
-  MinParty int32
-  MaxParty int32
-  MaxPlayers int32
-  RequiresGuardianOath bool
+	IsMatchmade          bool
+	MinParty             int32
+	MaxParty             int32
+	MaxPlayers           int32
+	RequiresGuardianOath bool
 }
 
 type DestinyActivityGuided struct {
-  MaxLobbySize int32 `json:"guidedMaxLobbySize"`
-  MinLobbySize int32 `json:"guidedMinLobbySize"`
-  DisbandCount int32 `json:"guidedDisbandCount"`
+	MaxLobbySize int32 `json:"guidedMaxLobbySize"`
+	MinLobbySize int32 `json:"guidedMinLobbySize"`
+	DisbandCount int32 `json:"guidedDisbandCount"`
 }
 
 type DestinyActivity struct {
-  ActivityLevel int32
-  ActivityLightLevel int32
-  ActivityTypeHash uint32
-  ActivityModeHashes []uint32
-  ActivityModeTypes []DestinyActivityMode
-  Challenges []DestinyActivityChallenges
-  DisplayProperties DestinyDisplayProperties
-  DestinationHash uint32
-  DirectActivityModeHash uint32
-  DirectActivityModeType DestinyActivityMode
-  GuidedGame DestinyActivityGuided
-  Matchmaking DestinyActivityMatchmaking
-  Modifiers []DestinyActivityModifier
-  OptionalUnlockStrings []DestinyActivityUnlockString
-  PCGRImage string
-  PlaceHash uint32
-  IsPlaylist bool
-  IsPVP bool
-  PlaylistItems []DestinyActivityPlaylistItem
-  ReleaseIcon string
-  ReleaseTimeEpoch int32 `json:"releaseTime"`
-  ReleaseTime time.Time `json:"-"`
-  Rewards []DestinyActivityReward
-  Redacted bool
-  Tier int32
+	ActivityLevel          int32
+	ActivityLightLevel     int32
+	ActivityTypeHash       uint32
+	ActivityModeHashes     []uint32
+	ActivityModeTypes      []DestinyActivityMode
+	Challenges             []DestinyActivityChallenges
+	DisplayProperties      DestinyDisplayProperties
+	DestinationHash        uint32
+	DirectActivityModeHash uint32
+	DirectActivityModeType DestinyActivityMode
+	GuidedGame             DestinyActivityGuided
+	Matchmaking            DestinyActivityMatchmaking
+	Modifiers              []DestinyActivityModifier
+	OptionalUnlockStrings  []DestinyActivityUnlockString
+	PCGRImage              string
+	PlaceHash              uint32
+	IsPlaylist             bool
+	IsPVP                  bool
+	PlaylistItems          []DestinyActivityPlaylistItem
+	ReleaseIcon            string
+	ReleaseTimeEpoch       int32     `json:"releaseTime"`
+	ReleaseTime            time.Time `json:"-"`
+	Rewards                []DestinyActivityReward
+	Redacted               bool
+	Tier                   int32
 }
 
 func manifestActivityDefinition(languageCode string, activityHash uint32) (*DestinyActivity, error) {
-  dataFile := fmt.Sprintf("%s-%s", manifestWorld, languageCode)
-  db, err := manifestOpenData(dataFile)
-  if err != nil { return nil, err }
+	dataFile := fmt.Sprintf("%s-%s", manifestWorld, languageCode)
+	db, err := manifestOpenData(dataFile)
+	if err != nil {
+		return nil, err
+	}
 
-  rows, err := db.Query("SELECT json FROM DestinyActivityDefinition WHERE id = $1", int32(activityHash))
-  if err != nil { return nil, err }
-  defer rows.Close()
-  if !rows.Next() { return nil, errors.New("No Results!") }
-  var jsonData string
-  err = rows.Scan(&jsonData)
-  if err != nil { return nil, err }
+	rows, err := db.Query("SELECT json FROM DestinyActivityDefinition WHERE id = $1", int32(activityHash))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	if !rows.Next() {
+		return nil, errors.New("No Results!")
+	}
+	var jsonData string
+	err = rows.Scan(&jsonData)
+	if err != nil {
+		return nil, err
+	}
 
-  result := DestinyActivity{}
-  err = json.Unmarshal([]byte(jsonData), &result)
-  if err != nil { return nil, err }
-  result.ReleaseTime = time.Unix(int64(result.ReleaseTimeEpoch), 0)
+	result := DestinyActivity{}
+	err = json.Unmarshal([]byte(jsonData), &result)
+	if err != nil {
+		return nil, err
+	}
+	result.ReleaseTime = time.Unix(int64(result.ReleaseTimeEpoch), 0)
 
-  return &result, nil
+	return &result, nil
 }
 
-func (base *DestinyMilestoneActivity)Definition(languageCode string) (*DestinyActivity, error) {
-  return manifestActivityDefinition(languageCode, base.ActivityHash)
+func (base *DestinyMilestoneActivity) Definition(languageCode string) (*DestinyActivity, error) {
+	return manifestActivityDefinition(languageCode, base.ActivityHash)
 }
 
-func (base *CharacterActivities)ActivityDefinition(languageCode string) (*DestinyActivity, error) {
-  return manifestActivityDefinition(languageCode, base.CurrentActivityHash)
+func (base *CharacterActivities) ActivityDefinition(languageCode string) (*DestinyActivity, error) {
+	return manifestActivityDefinition(languageCode, base.CurrentActivityHash)
+}
+
+func ActivityDefinitions(languageCode string, hashes []uint32) (*[]DestinyActivity, error) {
+	dataFile := fmt.Sprintf("%s-%s", manifestWorld, languageCode)
+	db, err := manifestOpenData(dataFile)
+	if err != nil {
+		return nil, err
+	}
+
+	params := make([]interface{}, len(hashes))
+	queryStr := "SELECT json FROM DestinyActivityDefinition WHERE id IN ("
+	for index, hash := range hashes {
+		params[index] = int32(hash)
+		if index+1 < len(hashes) {
+			queryStr += "?,"
+		} else {
+			queryStr += "?"
+		}
+	}
+	queryStr += ")"
+
+	rows, err := db.Query(queryStr, params...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var defns []DestinyActivity
+
+	for rows.Next() {
+		var jsonData string
+		err = rows.Scan(&jsonData)
+		if err != nil {
+			return &defns, err
+		}
+
+		result := DestinyActivity{}
+		err = json.Unmarshal([]byte(jsonData), &result)
+		if err != nil {
+			return &defns, err
+		}
+		result.ReleaseTime = time.Unix(int64(result.ReleaseTimeEpoch), 0)
+
+		defns = append(defns, result)
+	}
+
+	return &defns, nil
 }
